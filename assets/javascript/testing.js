5fdeacc0b83ac23f75f238e0036d1edbc6b04870
Mafalda <mafalda.villazan@gmail.com>
2019-07-02 10:07:26 -0700

    Changes on app and testing to showcase user input

--- assets/javascript/testing.js
+++ assets/javascript/testing.js
@@ -1,35 +1,46 @@
 //Document. ready
 $(document).ready(function () {
 
+    var ingredientArr = []
+    console.log(ingredientArr)
+    
+    function searchIngredient (){
+        var ingredient = $(this).attr("ingredient-user");
+            var queryURLEdamam = "https://api.edamam.com/search?q=" + ingredient + "&app_id=26194528&app_key=be9ed2ffac6143f2a60323088693f678"
+ 
+    
+        $.ajax({
+            url: queryURLEdamam,
+            method: "GET"
+        }).then(function (response) {
+            console.log("EDAMAM:  " + response)
+        });
+    }
 
 
-    var queryURLEdamam = "https://api.edamam.com/search?q=" + ingredients + "&app_id=26194528&app_key=be9ed2ffac6143f2a60323088693f678&from=0&to=3&calories=591-722&health=alcohol-free"
-
-
-    $.ajax({
-        url: queryURLEdamam,
-        method: "GET"
-    }).then(function (response) {
-        console.log("EDAMAM: ")
-        console.log(response)
-    });
-    //1. Take info from user
-    // Global variable for user input info
-    $(".material-icons").on('click', function (event) { // Search bar uses return key to search, so not sure if ".material-icons" is correct. 
+    $("#search").on('return', function(event){
         event.preventDefault();
-        var ingredient = $(".material-icons").val().trim();
-        var ingredientStr = String(ingredient);
-        console.log(ingredients);
+        var ingredient = $("#search").val();
+        // The movie from the textbox is then added to our array
+        ingredientArr.push(ingredient);
+        console.log(ingredientArr)
+        searchIngredient
     })
-// (use giphy film example)
-// Take user input and make into ingredients variable
-// On click function to pull ingredients from API to find recipe
 
-// 2. Displaying results
-// Create the cards that display recipe in HTML
-//Trim the the result
 
-//3. Store the ingredients from the selected recipe and search walmart api
-//4. Display results of walmart API search
+
+
+
+
+    // (use giphy film example)
+    // Take user input and make into ingredients variable
+    // On click function to pull ingredients from API to find recipe
+
+    // 2. Displaying results
+    // Create the cards that display recipe in HTML
+    //Trim the the result
+
+    //3. Store the ingredients from the selected recipe and search walmart api
+    //4. Display results of walmart API search
 
 });
